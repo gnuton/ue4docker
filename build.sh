952b19cc30aee1fbaecf1b7e33e64dc2af64b5a0
Antonio Aloisio <antonio.aloisio@zalando.fi>
2017-05-04 23:54:27 +0300

    Build has been renamed to createImage.sh and a new build command has been added

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-if [ -z "$OAUTH_TOKEN" ]; then
-    echo "Please set the github oauth token in the var OAUTH_TOKEN before building this image"
-    exit 1
-fi  
-
-docker build -t gnuton/ue4 . --build-arg OAUTH_TOKEN=$OAUTH_TOKEN
+mkdir $HOME/ue4
+docker run -it --rm -v $HOME/ue4:/opt/ue4 -w /opt/ue4 gnuton/ue4
